5846be764968c17a3a1ef70b9735c54a1c8af96e
Francesco Giambruno <frank96990@gmail.com>
2017-05-06 13:37:13 +0200

    Add functionalities
    Selection's query with parameters

--- dbManager.py
+++ dbManager.py
@@ -12,6 +12,8 @@ def menu():
 
 try:
   connection = mysql.connector.connect(user = '....', password = '....', database = '.....') # Your data
+  if(connection):
+      print("Connection enstablished!\n")
 except mysql.connector.Error as err:
  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
    print("The username or the password is wrong")
@@ -36,9 +38,23 @@ while(exit != -1):
     elif(choice == 2):
         cursor2 = connection.cursor()
         cursor2.execute("SELECT * FROM myDB") # Change this query with your data
-        for (name, surname) in cursor2:
-            print("\t{}, {}".format(name, surname)) # Change .format(v1, v2, ... , vn) with your variables
+        print("\n")
+        if not cursor2.rowcount:
+            print("The database is empty\n")
+        else:
+            for (name, surname) in cursor2: # Change "name" and "surname" with your variables
+                print("\t{}, {}".format(name, surname)) # Change .format(v1, v2, ... , vn) with your variables
         cursor2.close()
+        print("\n")
+    elif(choice == 3):
+        cursor3 = connection.cursor()
+        name = input("Insert name: ")
+        surname = input("Insert surname: ")
+        cursor3.execute("SELECT * FROM prova WHERE nome = %s OR cognome = %s", (name, surname)) # Change this query with your data
+        print("\n")
+        for (nome, cognome) in cursor3: # Change "name" and "surname" with your variables
+            print("\t{}, {}".format(nome, cognome)) # Change .format(v1, v2, ... , vn) with your variables
+        cursor3.close()
         print("\n")
     elif(choice == -1):
         exit = -1
